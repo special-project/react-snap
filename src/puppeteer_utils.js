653f9ab8e19ac33fa399f5f08a32fd848155f78d
pbtripletree <paul@tripletreellc.com>
2021-03-05 15:41:04 -0700

    set timeout in page.goto instead of puppeteer.launch

--- src/puppeteer_utils.js
+++ src/puppeteer_utils.js
@@ -207,7 +207,6 @@ const crawl = async (opt) => {
     executablePath: options.puppeteerExecutablePath,
     ignoreHTTPSErrors: options.puppeteerIgnoreHTTPSErrors,
     handleSIGINT: false,
-    timeout: 0,
   });
 
   /**
@@ -246,7 +245,7 @@ const crawl = async (opt) => {
         await page.setUserAgent(options.userAgent);
         const tracker = createTracker(page);
         try {
-          await page.goto(pageUrl, { waitUntil: "networkidle0" });
+          await page.goto(pageUrl, { waitUntil: "load", timeout: 0 });
         } catch (e) {
           e.message = augmentTimeoutError(e.message, tracker);
           throw e;
